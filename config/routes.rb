bd02ee9ac0c35da1d253daea4423672b6b2d7733
souen <souen_xx@yahoo.co.jp>
2019-08-25 15:30:24 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
 end
